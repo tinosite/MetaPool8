926258211f59576504f5e65fb479f29cb69312c8
navid manafi <manafinavid@yahoo.com>
2022-02-09 23:18:40 +0330

    Change NavBar Type

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,34 +1,33 @@
 import React from 'react'
-import { Nav, Button } from 'react-bootstrap';
+import { Nav, Button, Container, NavDropdown, Form, FormControl, Navbar as Nb } from 'react-bootstrap';
 import Link from 'next/link'
-import NavLink from './NavLink'
+import NavLink  from './NavLink'
 
 export default function Navbar() {
   return (
-    <Nav className="navbar navbar-expand-lg">
-  <div className="container-xl">
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    <Nb expand="lg" >
+    <Container fluid>
+      <Nb.Toggle aria-controls="navbarScroll" />
+      <Nb.Collapse id="navbarScroll">
+        <Nav
+          className="me-auto my-2 my-lg-0"
+          style={{ maxHeight: '100px' }}
+          navbarScroll
+          activeKey="/home"
+          onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+        >
 
-    <div className="collapse navbar-collapse" id="navbarsExample07XL">
-      <ul className="navbar-nav mr-auto">
+          <Nav.Link href="#action1" className="px-2 mx-4 active">Home</Nav.Link>
+          <Nav.Link href="#action2" className="px-2 mx-4">Video</Nav.Link>
+          <Nav.Link href="#action2" className="px-2 mx-4">Gameplay</Nav.Link>
+          <Nav.Link href="#action2" className="px-2 mx-4">Ntfs items</Nav.Link>
+          <Nav.Link href="#action2" className="px-2 mx-4">Metapool coin</Nav.Link>
+          <Nav.Link href="#action2" className="px-2 mx-4">Team</Nav.Link>
+          <Nav.Link href="#action2" className="px-2 mx-4">Roadmap</Nav.Link>
 
-        <NavLink href="/articles" title="Home" active={true}/>
-        <NavLink href="/articles" title="Video"/>
-        <NavLink href="/articles" title="Gameplay"/>
-        <NavLink href="/articles" title="Nfts item"/>
-        <NavLink href="/articles" title="Metapool coin"/>
-        <NavLink href="/articles" title="Team"/>
-        <NavLink href="/articles" title="Roadmap"/>
-
-
-
-
-      </ul>
-
-    </div>
-  </div>
-</Nav>
+        </Nav>
+    </Nb.Collapse>
+  </Container>
+</Nb>
   )
 }
