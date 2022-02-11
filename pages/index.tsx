af3ecdbeeb5a86bcd658f822eacd495b181482ae
navid manafi <manafinavid@yahoo.com>
2022-02-11 19:27:22 +0330

    Section1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,9 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import Section1 from '../components/sections/Section1'
 const Home: NextPage = () => {
   return (
     <>
-      <video playsInline={true} autoPlay={true} muted loop id="myVideo">
-          <source src="/first_page_background.mp4" type="video/mp4"/>
-      </video>
-      
-      <main className={styles.main}>
-        
-      </main>
+      <Section1></Section1>
     </>
   )
 }
