e4ecb34676097f5c910705ff88ed0498dfd908aa
navid manafi <manafinavid@yahoo.com>
2022-02-24 17:50:57 +0330

    Change3

--- components/sections/Section3.jsx
+++ components/sections/Section3.jsx
@@ -7,37 +7,37 @@ export default function Section3() {
   return (
         <>
             <main className={styles.Section}>
-                <Container className={styles.Section3ContentContainer}>
-                    <Row>
-                        <Col className="col-3" >
-                            <Row>
-                                <div className={[styles.PlayToEarn,"col-4 mx-0"].join(" ")}></div>
-                                <div className="col section3-lable p-0 section3-lable-left" ><lable glass="true">Play to earn</lable></div>
+                <Container>
+                    <Row className={styles.Section3ContentContainer}>
+                        <Col className="col-md-3 ms-row" >
+                            <Row className={"col"}>
+                                <div className={[styles.PlayToEarn,"col-md-4 mx-0"].join(" ")}></div>
+                                <div className="col-md-8 section3-lable p-0 section3-lable-left" ><lable glass="true">Play to earn</lable></div>
                             </Row>
-                            <Row>
-                                <div className={[styles.GiftBox,"col-4 mx-0"].join(" ")}></div>
-                                <div className="col section3-lable p-0 section3-lable-left" ><lable glass="true">Gift Box</lable></div>
+                            <Row className={"col"}>
+                                <div className={[styles.GiftBox,"col-md-4 mx-0"].join(" ")}></div>
+                                <div className="col-md-8 section3-lable p-0 section3-lable-left" ><lable glass="true">Gift Box</lable></div>
                             </Row>
-                            <Row>
-                                <div className={[styles.Upgrade,"col-4 mx-0"].join(" ")}></div>
-                                <div className="col section3-lable p-0 section3-lable-left" ><lable glass="true">UPgrade</lable></div>
+                            <Row className={"col"}>
+                                <div className={[styles.Upgrade,"col-md-4 mx-0"].join(" ")}></div>
+                                <div className="col-md-8 section3-lable p-0 section3-lable-left" ><lable glass="true">UPgrade</lable></div>
                             </Row>
                         </Col>
 
-                        <Col className={styles.Section3MidImage}></Col>
+                        <Col className={[styles.Section3MidImage,"col-mid-6 ms-row"].join(" ")}></Col>
                         
-                        <Col className="col-3" >
-                            <Row>
-                                <div className="col section3-lable section3-lable-right p-0" ><lable glass="true">Trade</lable></div>
-                                <div className={[styles.Trade,"col-4 mx-0"].join(" ")}></div>
+                        <Col className="col-md-3 ms-row" >
+                            <Row className={"col"} >
+                                <div className="col-md-8 section3-lable section3-lable-right p-0" ><lable glass="true">Trade</lable></div>
+                                <div className={[styles.Trade,"col-md-4 mx-0"].join(" ")}></div>
                             </Row>
-                            <Row>
-                                <div className="col section3-lable section3-lable-right p-0" ><lable glass="true">Stake</lable></div>
-                                <div className={[styles.Stake,"col-4 mx-0"].join(" ")}></div>
+                            <Row className={"col"}>
+                                <div className="col-md-8 section3-lable section3-lable-right p-0" ><lable glass="true">Stake</lable></div>
+                                <div className={[styles.Stake,"col-md-4 mx-0"].join(" ")}></div>
                             </Row>
-                            <Row>
-                                <div className="col section3-lable section3-lable-right p-0" ><lable glass="true">NTF Items</lable></div>
-                                <div className={[styles.NTF_Items,"col-4 mx-0"].join(" ")}></div>
+                            <Row className={"col"}>
+                                <div className="col-md-8 section3-lable section3-lable-right p-0" ><lable glass="true">NTF Items</lable></div>
+                                <div className={[styles.NTF_Items,"col-md-4 mx-0"].join(" ")}></div>
                             </Row>
                         </Col>
                     </Row>
