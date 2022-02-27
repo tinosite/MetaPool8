befa89a520174175c22ec9a0b337090cd8636edc
hadihabibtabar <hhtn555@gmail.com>
2022-02-27 17:28:24 +0330

    Merge branch 'main' of https://github.com/tinosite/MetaPool8

---
+++ components/sections/Section2.jsx
@@ -0,0 +1,77 @@
+import Image from 'next/image';
+import React from 'react'
+import { Row, Col, Container } from 'react-bootstrap';
+import styles from '../../styles/Home.module.scss'
+
+export default function Section2() {
+    return (
+        <>
+
+
+            <main className={styles.Section1}>
+                     <img src='/image/backslide2.jpg' id="backslide2" ></img>
+
+                
+                 <Container>
+                     
+                  {/*  <Row >
+
+                        <Col class="col-10">
+                            <div class="Section2_title">
+                                <span>
+                                    Game Play
+                                </span>
+                            </div>
+
+                        </Col>
+                    </Row>
+                    <Row>
+                        <div style={{ width: "1150px", borderRadius: "20px", height: "450px", marginTop: "100px", color: "white" }} glass="true">
+                            <Row>
+                                <Col class="col-lg-8 col-md-12 ">
+                                    <div class="imageplanet">
+                                        <a href="#" id="venus">
+                                            <img src='/image/Venus.png' alt=""></img>
+
+                                        </a>
+                                        <a href="#" id="earth">
+                                            <img src='/image/Earth.png' alt=""></img>
+
+                                        </a>
+                                        <a href="#" id="neptune">
+
+                                            <img src='/image/Neptune.png' alt=""></img>
+
+                                        </a>
+                                        <a href="#" id="mercury">
+
+                                            <img src='/image/Mercury.png' alt=""></img>
+
+                                        </a>
+                                        <a href="#" id="saturn">
+                                            <img src='/image/Saturn.png' alt=""></img>
+                                        </a>
+                                        <a href="#" id="mars">
+                                            <img src='/image/Mars.png' alt=""></img>
+                                        </a>
+
+                                    </div>
+
+                                </Col>
+                                <Col class="col-lg-4 col-md-12">
+                                    <div class="slideonetext">
+                                        <h2>Free Play</h2>
+                                        <hr></hr>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quod commodi amet odio, laudantium optio architecto minima aliquid, nulla, eveniet porro dolor suscipit enim. Provident at cum omnis tempora consequuntur eum iusto itaque voluptas vero corporis ea deserunt harum libero fugit officia aspernatur expedita, id aperiam alias repellat sed. Cumque?</p>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Row>*/}
+
+                  
+                </Container> 
+             </main >
+        </>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
 import Section1 from '../components/sections/Section1'
+import Section2 from '../components/sections/Section2'
 const Home: NextPage = () => {
   return (
     <>
       <Section1></Section1>
+      <Section2></Section2>
     </>
   )
 }
