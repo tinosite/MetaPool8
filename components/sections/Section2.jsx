bc3174cc70ef554bd3bf3d9f5e08ebaba7d57109
hadihabibtabar <hhtn555@gmail.com>
2022-03-04 20:00:10 +0330

    Section2

--- components/sections/Section2.jsx
+++ components/sections/Section2.jsx
@@ -1,77 +1,122 @@
-import Image from 'next/image';
 import React from 'react'
 import { Row, Col, Container } from 'react-bootstrap';
-import styles from '../../styles/Home.module.scss'
+import styles from './Section2.module.scss'
 
 export default function Section2() {
     return (
         <>
-
-
-            <main className={styles.Section1}>
-                     <img src='/image/backslide2.jpg' id="backslide2" ></img>
-
-                
-                 <Container>
-                     
-                  {/*  <Row >
-
-                        <Col class="col-10">
-                            <div class="Section2_title">
-                                <span>
-                                    Game Play
-                                </span>
-                            </div>
-
-                        </Col>
+            <section className={styles.Section}>
+                <img src="/image/backslide2.jpg" className={styles.Section2BackGround} />
+                <Container>
+                    <Row className={styles.Section2title}>
+                        <h2>Game Play</h2>
                     </Row>
-                    <Row>
-                        <div style={{ width: "1150px", borderRadius: "20px", height: "450px", marginTop: "100px", color: "white" }} glass="true">
-                            <Row>
-                                <Col class="col-lg-8 col-md-12 ">
-                                    <div class="imageplanet">
-                                        <a href="#" id="venus">
-                                            <img src='/image/Venus.png' alt=""></img>
+                    <Row className={styles.Section2Glass} glass="true">
 
-                                        </a>
-                                        <a href="#" id="earth">
-                                            <img src='/image/Earth.png' alt=""></img>
+                        {/*Start Slide one*/}
 
-                                        </a>
-                                        <a href="#" id="neptune">
+                        <Row id="slide1">
+                            <Col className={styles.slide1image} lg={8} sm={12}>
+                                <img src="/image/anime.png"></img>
+                            </Col>
+                            <Col className={styles.slide1text} lg={4} sm={12}>
+                                <h3>introduction</h3>
+                                <hr />
+                                <p>You can choose the planet you want and compete on it. To participate in the race, you have to pay a token according to the desired planet. We have different plans for you so that you can choose the desired planet according to the amount of tokens.
+                                    If you win the match, you will receive tokens from the losing side.  For example, if you participate in the earth match and pay 2x tokens and win, in addition to your own tokens, you will also receive 2x tokens from your opponent.</p>
+                            </Col>
+                        </Row>
 
-                                            <img src='/image/Neptune.png' alt=""></img>
+                        {/*End Slide 1*/}
+                        {/*Start Slide 2*/}
+                        {/* <Row id="slide2">
+                            <Col className={styles.slide2image} lg={8} sm={12}>
+                                <Row className={styles.firstPlanets}>
+                                    <a href="#" className={styles.venus} id="venus">
+                                    <img src='/image/Venus.png' alt=""></img>
 
-                                        </a>
-                                        <a href="#" id="mercury">
+                                </a>
+                                <a href="#" className={styles.earth} id="earth">
+                                    <img src='/image/Earth.png' alt=""></img>
 
-                                            <img src='/image/Mercury.png' alt=""></img>
+                                </a>
+                                <a href="#" className={styles.neptune} id="neptune">
 
-                                        </a>
-                                        <a href="#" id="saturn">
-                                            <img src='/image/Saturn.png' alt=""></img>
-                                        </a>
-                                        <a href="#" id="mars">
-                                            <img src='/image/Mars.png' alt=""></img>
-                                        </a>
+                                    <img src='/image/Neptune.png' alt=""></img>
 
-                                    </div>
+                                </a>
+                                </Row>
+                                <Row className={styles.secondPlanets}>
+                                     <a href="#" className={styles.mercury} id="mercury">
 
-                                </Col>
-                                <Col class="col-lg-4 col-md-12">
-                                    <div class="slideonetext">
-                                        <h2>Free Play</h2>
-                                        <hr></hr>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quod commodi amet odio, laudantium optio architecto minima aliquid, nulla, eveniet porro dolor suscipit enim. Provident at cum omnis tempora consequuntur eum iusto itaque voluptas vero corporis ea deserunt harum libero fugit officia aspernatur expedita, id aperiam alias repellat sed. Cumque?</p>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </div>
-                    </Row>*/}
+                                    <img src='/image/Mercury.png' alt=""></img>
 
-                  
-                </Container> 
-             </main >
+                                </a>
+                                <a href="#" className={styles.saturn} id="saturn">
+                                    <img src='/image/Saturn.png' alt=""></img>
+                                </a>
+                                <a href="#" className={styles.mars} id="mars">
+                                    <img src='/image/Mars.png' alt=""></img>
+                                </a>
+                                </Row>
+                               
+                            </Col>
+                            <Col className={styles.slide1text} lg={4} sm={12}>
+                                <h3>1 on 1</h3>
+                                <hr />
+                                <p>Welcome to the competitions This section is very exciting because you can earn more money. As in the previous sections, in this section we have considered different designs for you that you can participate in and compete. There will be 4-person and 8-person competitions and the winner of the competition will have all the tokens of his competitors. For example, if you participate in an 8-player tournament, you will receive 8x  tokens by paying x tokens and winning 4 games.</p>
+                            </Col>
+                        </Row> */}
+
+                        {/*End Slide 2*/}
+                        {/*Start Slide 3*/}
+
+                        {/* <Row id="slide3">
+                            <Col className={styles.slide3image} lg={8} sm={12}>
+                                <img src="/image/Winner.png"></img>
+                            </Col>
+                            <Col className={styles.slide1text} lg={4} sm={12}>
+                                <h3>Tournament</h3>
+                                <hr />
+                                <p>Welcome to the competitions This section is very exciting because you can earn more money. As in the previous sections, in this section we have considered different designs for you that you can participate in and compete. There will be 4-person and 8-person competitions and the winner of the competition will have all the tokens of his competitors. For example, if you participate in an 8-player tournament, you will receive 8x  tokens by paying x tokens and winning 4 games.</p>
+                            </Col>
+                        </Row> */}
+                        {/*End Slide 3*/}
+                        {/*Start Slide 3*/}
+
+                        {/* <Row id="slide4">
+                            <Col className={styles.slide4image} lg={8} sm={12}>
+                                <img src="/image/Freeplay.png"></img>
+                            </Col>
+                            <Col className={styles.slide1text} lg={4} sm={12}>
+                                <h3>Free Play</h3>
+                                <hr />
+                                <p>Stay alive with your friend or be the last man standing. Show your skillsStay alive with your friend or be the last man standing. Show your skillsStay alive with your friend or be the last man standing. Show your skillsStay  alive with your friend   alive with your friend  </p>
+                            </Col>
+                        </Row> */}
+                        {/*End Slide 4*/}
+
+
+
+                    </Row>
+                    <Row className={styles.pageNavigation}>
+                        <Col sm={2} className={styles.section2PreBut}>
+                            <button type="button" >
+
+                            </button>
+                        </Col>
+                        <Col sm={8} className={styles.slideLine}>
+                            <button type="button" id="slide1Line"></button>
+                            <button type="button" id="slide2Line"></button>
+                            <button type="button" id="slide3Line"></button>
+                            <button type="button" id="slide4Line"></button>
+                        </Col>
+                        <Col sm={2} className={styles.section2NextBut}><button type="button" ></button></Col>
+                    </Row>
+                </Container>
+            </section>
+
         </>
     )
 }
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Section1 from '../components/sections/Section1'
+import Section2 from '../components/sections/Section2'
 import Section3 from '../components/sections/Section3'
 import Section4 from '../components/sections/Section4'
 import Section5 from '../components/sections/Section5'
@@ -8,6 +9,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Section1></Section1>
+      <Section2></Section2>
       <Section3></Section3>
       <Section4></Section4>
       <Section5></Section5>
